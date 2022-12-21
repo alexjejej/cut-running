98eb7477eb6226bb2288a936dc5b07a658434aab
brauliogrc <brauliogrc95@gmail.com>
2022-12-21 07:03:16 -0600

    *Inicio de sesion con cuenta Google* Seguimiento de tutorial en   https://www.youtube.com/watch?v=xjsgRe7FTCU

--- app/src/main/java/com/raywenderlich/android/rwandroidtutorial/login/AuthActivity.kt
+++ app/src/main/java/com/raywenderlich/android/rwandroidtutorial/login/AuthActivity.kt
@@ -1,19 +1,34 @@
 package com.raywenderlich.android.rwandroidtutorial.login
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.raywenderlich.android.runtracking.R
 
 class AuthActivity : AppCompatActivity() {
-    lateinit var txtEmail: EditText
-    lateinit var txtPass: EditText
-    lateinit var btnLogin: Button
-    lateinit var btnSignUp: Button
+    lateinit var txtEmail:      EditText
+    lateinit var txtPass:       EditText
+    lateinit var btnLogin:      Button
+    lateinit var btnSignUp:     Button
+    lateinit var btnGoogleSignin: Button
+    lateinit var authLayout:    LinearLayout
+
+    private val GOOGLE_SIGN_IN = 100 // Identificador que se envia a "startActivityForResult" para asi recoger la respuesta de la autenticacion
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,20 +36,41 @@ class AuthActivity : AppCompatActivity() {
 
         // Setup
         setup()
+        session()
+    }
+
+    override fun onStart() { // Se invoca cada vez que se vuelva a mostrar la pantalla
+        super.onStart()
+        // Mostramos de nuevo el layout en caso de que hagamos un log out y regresemos a este activity (pantalla)
+        authLayout.visibility = View.VISIBLE // Hacemos visible el layout
+    }
+
+    /** Comprobacion de si existe una sesion activa **/
+    private fun session() {
+        val prefs: SharedPreferences = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email: String?      = prefs.getString("email", null)
+        val provider: String?   = prefs.getString("provider", null)
+
+        if ( email != null && provider != null ) {
+            authLayout.visibility = View.INVISIBLE // Hacemos invisible el layout
+            showHome(email ?: "", ProviderType.valueOf(provider ?: ""))
+        }
     }
 
     // TODO: Refactorizar metodo
     private fun setup() {
-        txtEmail = this.findViewById(R.id.txtEmail)
-        txtPass = this.findViewById(R.id.txtPassword)
-        btnLogin = this.findViewById(R.id.btnLogin)
-        btnSignUp = this.findViewById(R.id.btnSignUp)
+        txtEmail    = this.findViewById(R.id.txtEmail)
+        txtPass     = this.findViewById(R.id.txtPassword)
+        btnLogin    = this.findViewById(R.id.btnLogin)
+        btnSignUp   = this.findViewById(R.id.btnSignUp)
+        btnGoogleSignin = this.findViewById(R.id.btnGoogleSignIn)
+        authLayout  = this.findViewById(R.id.authLayout)
 
         title = "Authenticcion" // Modificamos el titulo de la pantalla
 
         // Boton de signup
         btnSignUp.setOnClickListener {
-            if ( !(txtEmail.text.isNotEmpty() && txtPass.text.isNotEmpty()) ) {
+            if ( txtEmail.text.isNotEmpty() && txtPass.text.isNotEmpty() ) {
                 FirebaseAuth.getInstance()
                     .createUserWithEmailAndPassword(txtEmail.text.toString(), txtPass.text.toString()) // Crea un usuario con las credenciales dadas como parametros
                     .addOnCompleteListener {
@@ -69,6 +105,24 @@ class AuthActivity : AppCompatActivity() {
                     }
             }
         }
+
+        // Boton de Google sign in
+        btnGoogleSignin.setOnClickListener {
+            // Configuracion de login con Google
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN) // Indica que se usara el login por defecto de la cuenta de google
+                .requestIdToken(getString(R.string.default_web_client_id)) // Pasamos el token Id asociado a nuestra app
+                .requestEmail() // Solicitamos el email de la persona que se esta logueando
+                .build()
+
+            // Cliente de autenticacion de google
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut() // Para que cada vez que se inicie sesion con google se realice el log out de la cuenta que pueda estar autenticada. Esto
+            // es util para el caso de que se tenga mas de una cuenta de google asociada al dispositivo
+
+            // Mostrar la pantalla de autenticacion de Google
+            // val getContent = registerForActivityResult(googleClient.signInIntent)
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN) // TODO: Buscar implementacion reciente
+        }
     }
 
     /** Muestra una laerta de error **/
@@ -89,5 +143,39 @@ class AuthActivity : AppCompatActivity() {
             putExtra("provider", provider.name)
         }
         startActivity(homeIntent)
+    }
+
+    /** Deprecated **/
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) { // TODO: Consultar documentacion sober onActivityResult
+        super.onActivityResult(requestCode, resultCode, data)
+        if ( requestCode == GOOGLE_SIGN_IN ) {
+            // Si el request code es igual al reques code definido en GOOGLE_SIGN_IN querra decir
+            // que la respuesta de este activity se corresponde con el de la autenticacion con Google
+            if ( requestCode == GOOGLE_SIGN_IN ) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+                try {
+                    // Esta linea puede producir un error en caso de que no sea capaz de recuperar una cuenta
+                    val account = task.getResult(ApiException::class.java)
+
+                    if ( account != null ) {
+                        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                        FirebaseAuth.getInstance()
+                            .signInWithCredential(credential) // Pasamos la autenticacion a Firebase para crear un registro
+                            .addOnCompleteListener{
+                                if ( it.isSuccessful ) {
+                                    showHome(account.email ?: "", ProviderType.GOOGLE)
+                                }
+                                else {
+                                    showAlert()
+                                }
+                            }
+                    }
+                }
+                catch ( ex: ApiException ) {
+                    showAlert()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/raywenderlich/android/rwandroidtutorial/login/HomeActivity.kt
+++ app/src/main/java/com/raywenderlich/android/rwandroidtutorial/login/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.raywenderlich.android.rwandroidtutorial.login
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,9 +11,10 @@ import com.google.firebase.auth.FirebaseAuth
 import com.raywenderlich.android.runtracking.R
 
 enum class ProviderType {
-    /** De momento tiene le valor BASIC, ya que es el unico tipo de autenticacion que se tiene
+    /** Nos sirve para designar que metodo de autenticacion estamos utilizando
+     * De momento tiene le valor BASIC, ya que es el unico tipo de autenticacion que se tiene
      * el cual es una autenticacion basica por email y password **/
-    BASIC
+    BASIC, GOOGLE;
 }
 
 class HomeActivity : AppCompatActivity() {
@@ -26,10 +29,21 @@ class HomeActivity : AppCompatActivity() {
         // Recuperacion de los paramaetros enviados desde el login
         var bundle = intent.extras
         var email = bundle?.getString("email")
-        var pass = bundle?.getString("provider").toString()
+        var provider = bundle?.getString("provider").toString()
 
         // Setup
-        setup( email ?: "", pass ?:"" )
+        setup( email ?: "", provider ?:"" )
+
+        /** Guardado de los datos del usuario que se ha autenticado a nivel de sesion de la app
+         * para que si el usuario ya se encuentra autenticado no nos pida iniciar sesion
+         * de nuevo en el login**/
+        val prefs: SharedPreferences = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE) // Se lee el name el file desde el archivo strings.xml
+        with( prefs.edit() ) {
+            // Es preferible esitar las preferencias dentro de un bloque "with"
+            putString("email", email)
+            putString("provider", provider)
+            apply()
+        }
     }
 
     private fun setup( email: String, provider: String ) {
@@ -44,6 +58,13 @@ class HomeActivity : AppCompatActivity() {
 
         // Boton de logout
         btnLogout.setOnClickListener{
+            // Borrado de datos
+            val prefs: SharedPreferences = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+            with( prefs.edit() ) {
+                clear() // Borra todas las preferencias que se encuentran guardadas en el archivo
+                apply()
+            }
+
             FirebaseAuth.getInstance().signOut()
             onBackPressed() // Nos devuelve a la pantalla anterior, en este caso el login (AuthActivity)
         }
