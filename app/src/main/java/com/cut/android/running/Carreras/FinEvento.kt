10f0d26f8d7f580b8d00e3cb79b99dc252833aea
alexjejej <alejandro.garana@alumnos.udg.mx>
2024-03-14 13:38:16 -0600

    Activity a lanzar si hay un evento en curso

---
+++ app/src/main/java/com/cut/android/running/Carreras/FinEvento.kt
@@ -0,0 +1,12 @@
+package com.cut.android.running.Carreras
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cut.android.running.R
+
+class FinEvento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fin_evento)
+    }
+}
